ec5c56a0633c9a07bfe01a29d5acf4a6a046ff29
Jaime Alejandro Perez <jperezh3@gmail.com>
2018-02-11 21:42:14 +0100

    Playing with database.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const cool = require('cool-ascii-faces');
+const cool = require('cool-ascii-faces')
+const mongo = require('mongodb').MongoClient;
+
+
+const databaseName = 'heroku_8zdr4tt7';
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -9,4 +13,25 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (request, response) => response.send(cool()))
+  .get('/times', (req, res) => {
+		let result = '';
+		let times = process.env.TIMES || 5;
+		console.log(times);
+		for(var i=0; i < times; i++) {
+			result += i + ' ';
+		}
+		res.send(result);
+  })
+  .get('/db', (req, res) => {
+  		console.log(process.env.MONGODB_URI)
+		mongo.connect(process.env.MONGODB_URI, (err, client) => {
+			console.log('Succesfully connected to the database')
+			const dbo = client.db(databaseName)
+			dbo.createCollection('customers', (err, res) => {
+				if (err) throw err;
+				console.log('Collection created!!');
+				client.close();
+			})
+		})
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
